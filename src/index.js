b6fc281901e2b469bba882243c0f8508d7b2d2f2
sourav78 <souravranjan488@gmail.com>
2025-10-26 15:13:29 +0530

    Added logger - morgan library

--- src/index.js
+++ src/index.js
@@ -6,12 +6,28 @@ import { connectMongoDb } from "./db/MongoDBConnect.js"
 import { createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from "./controllers/ProductControllers.js"
 import redisClient from "./utils/RedisConnection.js"
 dotenv.config()
+import morgan from "morgan"
+import fs from 'fs';
+import path from 'path';
 
 // Establish mongodb connection
 connectMongoDb()
 
 // Initiate express app
 const app = express()
+
+// --- Setup Morgan Logger ---
+// 2. Create a write stream (in 'append' mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(process.cwd(), 'access.log'),
+  { flags: 'a' }
+);
+
+// 3. Setup the logger to use the 'combined' format and write to the file
+app.use(morgan('combined', { stream: accessLogStream }));
+
+// 4. (Optional) Log to console in 'dev' format
+app.use(morgan('dev'));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
